17f39898442c543cb5c8abb496b248ecf7fed960
Josh Hodgetts <101507209+DaddyDoughnut@users.noreply.github.com>
2023-05-01 14:28:36 +1000

    4# prefabed all scene objects

---
+++ Assets/Scripts/detectPlaneScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectPlaneScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        
+            
+            Debug.Log("bang");
+        
+        
+    }
+}
