0749f7a4637f396234c8d1b1b4356632d59492c8
Josh Hodgetts <101507209+DaddyDoughnut@users.noreply.github.com>
2023-06-03 16:49:59 +1000

    13# block movement and rotation detection complete

--- Assets/Scripts/rotatorScript.cs
+++ Assets/Scripts/rotatorScript.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class rotatorScript : MonoBehaviour
 {
-    private GameObject blocks;
+    public GameObject blocks;
 
     public bool hasCollided = false;
 
+    public GameObject GHOSTS;
+
     void Start(){
         blocks = GameObject.Find("BLOCKS");
+        GHOSTS = transform.Find("GHOSTS").gameObject;
+
     }
 
 
@@ -35,28 +39,109 @@ public class rotatorScript : MonoBehaviour
 
     }
 
+    
+
     void r(){
-        if(!transform.parent.gameObject.CompareTag("block")){
+        bool canMove = true;
+
+        GHOSTS.transform.Rotate(90f, 0f, 0f, Space.World);
+        foreach(Transform ghost in GHOSTS.transform){
+            if(Mathf.Abs(ghost.position.x) > 10.1 || Mathf.Abs(ghost.position.z) > 10.1 || ghost.position.y < 5.1){
+                canMove = false;
+                break;
+            }
+
+            foreach (Transform block in blocks.transform){
+                if (Mathf.Abs(block.position.x - ghost.position.x) < 10f && Mathf.Abs(block.position.y - ghost.position.y) < 10f && Mathf.Abs(block.position.z - ghost.position.z) < 10f){
+                    canMove = false;
+                    break;
+                }
+            }
+            if (!canMove)
+            {
+                break;
+            }
+
+        }
+        GHOSTS.transform.Rotate(-90f, 0f, 0f, Space.World);
+        if (canMove){
             transform.Rotate(90f, 0f, 0f, Space.World);
-        } 
+        }
+
+
     }
+
     void t(){
-        if(!transform.parent.gameObject.CompareTag("block")){
+        bool canMove = true;
+
+        GHOSTS.transform.Rotate(0f, 90f, 0f, Space.World);
+        foreach(Transform ghost in GHOSTS.transform){
+            if(Mathf.Abs(ghost.position.x) > 10.1 || Mathf.Abs(ghost.position.z) > 10.1 || ghost.position.y < 5.1){
+                canMove = false;
+                break;
+            }
+
+            foreach (Transform block in blocks.transform){
+                if (Mathf.Abs(block.position.x - ghost.position.x) < 10f && Mathf.Abs(block.position.y - ghost.position.y) < 10f && Mathf.Abs(block.position.z - ghost.position.z) < 10f){
+                    canMove = false;
+                    break;
+                }
+            }
+            if (!canMove)
+            {
+                break;
+            }
+
+        }
+        GHOSTS.transform.Rotate(0f, -90f, 0f, Space.World);
+        if (canMove){
             transform.Rotate(0f, 90f, 0f, Space.World);
-        } 
+        }
     }
     void y(){
-        if(!transform.parent.gameObject.CompareTag("block")){
+        bool canMove = true;
+
+        GHOSTS.transform.Rotate(0f, 0f, 90f, Space.World);
+        foreach(Transform ghost in GHOSTS.transform){
+            if(Mathf.Abs(ghost.position.x) > 10.1 || Mathf.Abs(ghost.position.z) > 10.1 || ghost.position.y < 5.1){
+                canMove = false;
+                break;
+            }
+
+            foreach (Transform block in blocks.transform){
+                if (Mathf.Abs(block.position.x - ghost.position.x) < 10f && Mathf.Abs(block.position.y - ghost.position.y) < 10f && Mathf.Abs(block.position.z - ghost.position.z) < 10.1f){
+                    canMove = false;
+                    break;
+                }
+            }
+            if (!canMove)
+            {
+                break;
+            }
+
+        }
+        GHOSTS.transform.Rotate(0f, 0f, -90f, Space.World);
+        if (canMove){
             transform.Rotate(0f, 0f, 90f, Space.World);
-        } 
+        }
     }
 
     public void MoveChildrenToTarget()
     {
         foreach (Transform child in transform)
         {
-            child.gameObject.tag = "block";
-            child.SetParent(blocks.transform);
+            if (child != null)
+            {
+                if (child.gameObject.name == "GHOSTS")
+                {
+                    Destroy(child.gameObject);
+                }
+                else
+                {
+                    child.gameObject.tag = "block";
+                    child.SetParent(blocks.transform);
+                }
+            }
         }
     }
 }
