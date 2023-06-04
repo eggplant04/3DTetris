c378606510d878c1803916e98eb75ee47082e6c0
Josh Hodgetts <101507209+D4DDYD0NU7@users.noreply.github.com>
2023-06-04 23:43:50 +1000

    21# working mute button

--- Assets/Scripts/canvasScript.cs
+++ Assets/Scripts/canvasScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class canvasScript : MonoBehaviour
 {
@@ -15,10 +16,17 @@ public class canvasScript : MonoBehaviour
     public int levelLBLTextInt = 1;
     public int numOfClears = 0;
 
+    public Sprite sprite1; 
+    public Sprite sprite2;
+    public Image muteBTN; 
+    private bool isSprite1Active = true;
+    private GameObject boombox;  
+    private AudioSource audioPlayer;
+
     private void Update()
     {
         scoreLBLText.text = scoreLBLTextInt.ToString(); 
-        if (numOfClears == 3){
+        if (numOfClears == 4){
             numOfClears = 0;
             levelLBLTextInt += 1;
             levelLBLText.text = levelLBLTextInt.ToString();
@@ -27,7 +35,8 @@ public class canvasScript : MonoBehaviour
 
     private void Start()
     {
-        
+        boombox = GameObject.Find("BGMusicPlayer");
+        audioPlayer = boombox.GetComponent<AudioSource>();
         playPauseBTNString = playPauseBTNText.text;
     }
 
@@ -43,4 +52,14 @@ public class canvasScript : MonoBehaviour
         }
         playPauseBTNString = playPauseBTNText.text;
     }
+
+    
+    public void onMuteBTNPressed()
+    {
+        isSprite1Active = !isSprite1Active;
+
+        muteBTN.sprite = isSprite1Active ? sprite1 : sprite2;
+        audioPlayer.volume = isSprite1Active ? 1.0f : 0.0f;
+    }
+
 }
