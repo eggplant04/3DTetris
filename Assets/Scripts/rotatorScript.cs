7a889e61a6fb5591b77e78088c8d1f11a585adf1
Josh Hodgetts <101507209+D4DDYD0NU7@users.noreply.github.com>
2023-06-21 17:53:12 +1000

    31# blocks will now rotate against wall by moving when able

--- Assets/Scripts/rotatorScript.cs
+++ Assets/Scripts/rotatorScript.cs
@@ -47,6 +47,7 @@ public class rotatorScript : MonoBehaviour
     
 
     void rotateX(){
+        float ghostsMoved = 0f;
         if (GHOSTS != null && myCanvasScript.playPauseBTNString == "PAUSE"){
 
         
@@ -54,13 +55,31 @@ public class rotatorScript : MonoBehaviour
 
             GHOSTS.transform.Rotate(90f, 0f, 0f, Space.World);
             foreach(Transform ghost in GHOSTS.transform){
-                if(Mathf.Abs(ghost.position.x) > 10.1 || Mathf.Abs(ghost.position.z) > 10.1 || ghost.position.y < 5.1){
+                if(Mathf.Abs(ghost.position.z) > 10.1){
+                    
+                    if(ghost.position.z > 0){
+                        ghostsMoved -= 10f;
+                    }
+                    else{
+                        ghostsMoved += 10f;
+                    }
+                    
+                    break;
+                }
+                
+                else if (ghost.position.y < 5.1) {
                     canMove = false;
                     break;
                 }
 
+                
+
+            }
+            foreach(Transform ghost in GHOSTS.transform){
+                
+                
                 foreach (Transform block in blocks.transform){
-                    if (Mathf.Abs(block.position.x - ghost.position.x) < 10f && Mathf.Abs(block.position.y - ghost.position.y) < 10f && Mathf.Abs(block.position.z - ghost.position.z) < 10f){
+                    if (Mathf.Abs(block.position.x - ghost.position.x) < 10f && Mathf.Abs(block.position.y - ghost.position.y) < 10f && Mathf.Abs(block.position.z - (ghost.position.z + ghostsMoved)) < 10f){
                         canMove = false;
                         break;
                     }
@@ -69,30 +88,64 @@ public class rotatorScript : MonoBehaviour
                 {
                     break;
                 }
-
             }
+
             GHOSTS.transform.Rotate(-90f, 0f, 0f, Space.World);
+
             if (canMove){
+                
                 transform.Rotate(90f, 0f, 0f, Space.World);
+                transform.parent.Translate(new Vector3(0f, 0f, ghostsMoved));
             }
 
 
         }
     }
 
+
     void rotateZ(){
+        float ghostsMovedX = 0f;
+        float ghostsMovedZ = 0f;
         if (GHOSTS != null && myCanvasScript.playPauseBTNString == "PAUSE"){
+
+       
             bool canMove = true;
 
             GHOSTS.transform.Rotate(0f, 90f, 0f, Space.World);
             foreach(Transform ghost in GHOSTS.transform){
-                if(Mathf.Abs(ghost.position.x) > 10.1 || Mathf.Abs(ghost.position.z) > 10.1 || ghost.position.y < 5.1){
-                    canMove = false;
+                if(Mathf.Abs(ghost.position.x) > 10.1){
+                    
+                    
+                    if(ghost.position.x > 0){
+                        ghostsMovedX -= 10f;
+                    }
+                    else{
+                        ghostsMovedX += 10f;
+                    }
+                    
                     break;
                 }
+                else if (Mathf.Abs(ghost.position.z) > 10.1){
+                    
+                    
+                    if(ghost.position.z > 0){
+                        ghostsMovedZ -= 10f;
+                    }
+                    else{
+                        ghostsMovedZ += 10f;
+                    }
+                }
+                
+                
 
+                
+            
+            }
+            foreach(Transform ghost in GHOSTS.transform){
+                
+                
                 foreach (Transform block in blocks.transform){
-                    if (Mathf.Abs(block.position.x - ghost.position.x) < 10f && Mathf.Abs(block.position.y - ghost.position.y) < 10f && Mathf.Abs(block.position.z - ghost.position.z) < 10f){
+                    if (Mathf.Abs(block.position.x - (ghost.position.x + ghostsMovedX)) < 10f && Mathf.Abs(block.position.y - ghost.position.y) < 10f && Mathf.Abs(block.position.z - (ghost.position.z + ghostsMovedZ)) < 10f){
                         canMove = false;
                         break;
                     }
@@ -101,14 +154,20 @@ public class rotatorScript : MonoBehaviour
                 {
                     break;
                 }
+            }
 
-            }
             GHOSTS.transform.Rotate(0f, -90f, 0f, Space.World);
+
             if (canMove){
+                
                 transform.Rotate(0f, 90f, 0f, Space.World);
+                transform.parent.Translate(new Vector3(ghostsMovedX, 0f, ghostsMovedZ));
             }
-        }   
+            
+
+        }
     }
+    
         
     
 
